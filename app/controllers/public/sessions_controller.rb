389fe17d347617ef7b18d8fd80f7a817cbf5caa5
EC2 Default User <ec2-user@ip-172-31-0-103.ap-northeast-1.compute.internal>
2023-05-03 05:39:17 +0000

    更新

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -5,7 +5,7 @@ class Public::SessionsController < Devise::SessionsController
   before_action :configure_permitted_parameters, only: [:create]
 
   def after_sign_in_path_for(resource)
-    customer_path(current_customer.id)
+    posts_path
   end
 
   def after_sign_out_path_for(resource)
@@ -19,7 +19,7 @@ class Public::SessionsController < Devise::SessionsController
   # POST /resource/sign_in
   # def create
   # super
-  # end
+  #end
 
   # DELETE /resource/sign_out
   # def destroy
